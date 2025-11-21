2e6a6c324c0c60dbcf354fdf7ff39c9460332378
elazdi-al <alielazdi@gmail.com>
2025-11-21 11:11:16 +0100

    feat: game loop, white background

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
-use std::io::{Write, stdout};
-use termion::{cursor::Goto, terminal_size};
+use std::{
+    io::{Write, stdout},
+    thread::sleep,
+    time::{self, Duration, Instant},
+};
+use termion::{clear, color, cursor::Goto, terminal_size};
+const WAIT: Duration = Duration::from_millis(10); //wait between frames
 
 fn main() {
     let mut stdout = stdout();
     let (width, height) = terminal_size().unwrap();
-    for y in 1..=height {
-        for x in 1..=width {
-            write!(stdout, "{}+", Goto(x, y)).unwrap();
+
+    loop {
+        write!(stdout, "{}", color::Bg(color::White)).unwrap();
+
+        for y in 1..=height {
+            for x in 1..=width {
+                write!(stdout, "{} ", Goto(x, y)).unwrap();
+            }
         }
+
+        stdout.flush().unwrap();
+        write!(stdout, "{}{}", clear::All, color::Bg(color::Reset)).unwrap();
+        sleep(WAIT);
     }
-    stdout.flush().unwrap();
 }
