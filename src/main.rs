f8bf22e887f901a652f0619a12cb2415d0962e0a
elazdi-al <alielazdi@gmail.com>
2025-11-21 10:52:04 +0100

    feat: draw + on all screen

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 use std::io::{Write, stdout};
-use termion::cursor::Goto;
+use termion::{cursor::Goto, terminal_size};
 
 fn main() {
     let mut stdout = stdout();
-
-    let width = 20;
-    let height = 10;
-
+    let (width, height) = terminal_size().unwrap();
     for y in 1..=height {
         for x in 1..=width {
             write!(stdout, "{}+", Goto(x, y)).unwrap();
