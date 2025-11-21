09890dcb6ee8e7b895468f256d8e8a4bd57c9edf
elazdi-al <alielazdi@gmail.com>
2025-11-21 10:50:31 +0100

    feat: add basic termion example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::{Write, stdout};
+use termion::cursor::Goto;
+
 fn main() {
-    println!("Hello, world!");
+    let mut stdout = stdout();
+
+    let width = 20;
+    let height = 10;
+
+    for y in 1..=height {
+        for x in 1..=width {
+            write!(stdout, "{}+", Goto(x, y)).unwrap();
+        }
+    }
+    stdout.flush().unwrap();
 }
